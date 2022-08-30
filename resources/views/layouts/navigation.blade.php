7c0a214706aff55d2f81e1769cabb4c7c35e8f92
mamemaru00 <k.shouhei0711@gmail.com>
2022-08-30 16:27:52 +0900

    日本語化対応

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -18,9 +18,9 @@
                 </div>
             </div>
 
-            <!-- Settings Dropdown -->
+            <!-- Settings Dropdown
             <div class="hidden sm:flex sm:items-center sm:ml-6">
-                <x-dropdown align="right" width="48">
+                <x-dropdown align-right="right" width="48">
                     <x-slot name="trigger">
                         <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
                             <div>{{ Auth::user()->name }}</div>
@@ -31,7 +31,7 @@
                                 </svg>
                             </div>
                         </button>
-                    </x-slot>
+                    </x-slot> -->
 
                     <x-slot name="content">
                         <!-- Authentication -->
