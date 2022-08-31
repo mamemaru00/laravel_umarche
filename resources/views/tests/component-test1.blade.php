8c011eacd045815fe68c8f660cf633f0eacc9012
mamemaru00 <k.shouhei0711@gmail.com>
2022-08-31 12:07:41 +0900

    props

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -2,5 +2,6 @@
 <x-slot name="header">ヘッダー1</x-slot>
         コンポーネント1
 
-  <x-tests.card title="タイトル" content="本文" />      
+  <x-tests.card title="タイトル1" content="本文" /> 
+  <x-tests.card title="タイトル2"  />      
 </x-tests.app>
