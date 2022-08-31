b46ef900c64f98c717c1a3fdffd5895f961569fc
mamemaru00 <k.shouhei0711@gmail.com>
2022-08-31 14:26:58 +0900

    classBase

---
+++ resources/views/components/tests/test-class-base-component.blade.php
@@ -0,0 +1,6 @@
+<div>
+    クラスすベースのコンポーネントです
+    <div>{{ $classBaseMessage }}</div>
+    <div>{{ $defaultMessage }}</div>
+    <!-- Do what you can, with what you have, where you are. - Theodore Roosevelt -->
+</div>
