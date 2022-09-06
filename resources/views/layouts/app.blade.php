df19ace5b657e7ab9e9012d70dd99794217389c6
mamemaru00 <k.shouhei0711@gmail.com>
2022-09-06 14:56:25 +0900

    index, create

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,8 +11,9 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Scripts -->
-        <link rel="stylesheet" href="{{asset('css/app.css')}}">
-        
+        <!-- <link rel="stylesheet" href="{{asset('css/app.css')}}"> -->
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <!-- <link rel="stylesheet" href="{{ asset('css/app.css?20220906') }}"> -->
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
