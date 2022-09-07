fff5950c245fd08bec496ab675ac73102a179f04
mamemaru00 <k.shouhei0711@gmail.com>
2022-09-07 11:33:17 +0900

    pagination

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -36,7 +36,8 @@ class OwnersController extends Controller
         // var_dump($q_first);
 
         // dd($e_all,$q_get,$q_first,$c_test);
-        $owners = Owner::select('id', 'name', 'email', 'created_at')->get();
+        $owners = Owner::select('id', 'name', 'email', 'created_at')
+        ->paginate(3);
 
         return view('admin.owners.index', 
         compact('owners'));
@@ -136,7 +137,7 @@ class OwnersController extends Controller
         return redirect()
         ->route('admin.owners.index')
         ->with(['message' => 'オーナー情報を削除しました。',
-    'status' => 'alert']);
+        'status' => 'alert']);
     }
 
     public function expiredOwnerIndex()
