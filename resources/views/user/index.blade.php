dba7ab4e288ee8e9196a3e88373e5a419793fb96
mamemaru00 <k.shouhei0711@gmail.com>
2022-09-20 14:40:24 +0900

    view調整

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -10,18 +10,22 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <div class="flex flex-wrap">
-                        
+
                         @foreach($products as $product)
                         <div class="w-1/4 p-2 md:p-4">
                             <a href="">
                                 <div class="border rounded-md p-2 md:p-4">
                                     <x-thumbnail filename="{{$product->imageFirst->filename ?? ''}}" type="products" />
-                                    <div class="text-gray-700">{{ $product->name }}</div>
+                                    <div class="mt-4">
+                                        <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">{{ $product->category->name }}</h3>
+                                        <h2 class="text-gray-900 title-font text-lg font-medium">{{ $product->name }}The Catalyzer</h2>
+                                        <p class="mt-1">{{ number_format($product->price) }}<span class="text-sm text-gray-700">円(税込)</span></p>
+                                    </div>
                                 </div>
                             </a>
                         </div>
                         @endforeach
-                        
+
                     </div>
                 </div>
             </div>
