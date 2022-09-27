ed8a6f4730c76411df5f7f575e277ad2c0abe461
mamemaru00 <k.shouhei0711@gmail.com>
2022-09-27 12:57:04 +0900

    オーナー向けメール送信

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -11,6 +11,7 @@ use App\Models\Stock;
 use Stripe;
 use App\Service\CartService;
 use App\Jobs\SendThanksMail;
+use App\Jobs\SendOrderedMail;
 
 class CartController extends Controller
 {
@@ -60,15 +61,6 @@ class CartController extends Controller
 
     public function checkout()
     {
-        ////
-        $items = Cart::where('user_id', Auth::id())->get();
-        $products = CartService::getItemsInCart($items);
-        $user = User::findOrFail(Auth::id());
-
-        SendThanksMail::dispatch($products, $user);
-        dd('ユーザーメール送信テスト');
-        ////
-
         $user = User::findOrFail(Auth::id());
         $products = $user->products;
 
@@ -116,6 +108,19 @@ class CartController extends Controller
 
     public function success()
     {
+        ////
+        $items = Cart::where('user_id', Auth::id())->get();
+        $products = CartService::getItemsInCart($items);
+        $user = User::findOrFail(Auth::id());
+
+        SendThanksMail::dispatch($products, $user);
+        foreach($products as $product)
+        {
+            SendOrderedMail::dispatch($product, $user);
+        }
+        // dd('ユーザーメール送信テスト');
+        ////
+
         Cart::where('user_id', Auth::id())->delete();
 
         return redirect()->route('user.items.index');
